b05bfb7bbdd4c010c97e739b68267cc77ff34f05
eric-klimowich <eklimowich@hotmail.com>
2019-08-12 13:46:43 -0400

    Initial setup of task manager app, testing connection to mongodb database

---
+++ mongodb.js
@@ -0,0 +1,15 @@
+// CRUD Create Read Update Delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database.')
+    }
+
+    console.log('Connected correctly!')
+})
