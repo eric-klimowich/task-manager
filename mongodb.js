7da48a8960c030d2a7b6efc58c50a93714e3661c
eric-klimowich <eklimowich@hotmail.com>
2019-08-12 14:18:53 -0400

    Added a single user to the collection 'users' in the task-manager database

--- mongodb.js
+++ mongodb.js
@@ -11,5 +11,11 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database.')
     }
 
-    console.log('Connected correctly!')
+    // console.log('Connected correctly!')
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Eric',
+        age: 43
+    })
 })
