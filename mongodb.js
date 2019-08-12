301dada980fcbe1ccda225b0212d57fd56731d27
eric-klimowich <eklimowich@hotmail.com>
2019-08-12 16:11:08 -0400

    Working with IDs

--- mongodb.js
+++ mongodb.js
@@ -8,6 +8,13 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID()
+console.log(id)
+console.log(id.id)
+console.log(id.id.length)
+console.log(id.toHexString().length)
+console.log(id.getTimestamp())
+
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database.')
