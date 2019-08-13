1366236f84430dadc667ae3780c4d1498ec746a0
eric-klimowich <eklimowich@hotmail.com>
2019-08-13 11:31:59 -0400

    Created get request to find a single task by id

--- src/index.js
+++ src/index.js
@@ -64,7 +64,17 @@ app.get('/tasks', (req, res) => {
 })
 
 app.get('/tasks/:id', (req, res) => {
-    
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
+    })
 })
 
 app.listen(port, () => {
