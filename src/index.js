c1127e7300c0ca34e6fe104e89e31477d9f4a9bc
eric-klimowich <eklimowich@hotmail.com>
2019-08-13 13:46:18 -0400

    Refactored calls to task database using async and await

--- src/index.js
+++ src/index.js
@@ -77,39 +77,74 @@ app.get('/users/:id', async (req, res) => {
 //     })
 // })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    // console.log(req.body)
-    // res.send('testing!')
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+// app.post('/tasks', (req, res) => {
+//     const task = new Task(req.body)
+//     // console.log(req.body)
+//     // res.send('testing!')
+
+//     task.save().then(() => {
+//         res.status(201).send(task)
+//     }).catch((error) => {
+//         res.status(400).send(error)
+//     })
+// })
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((error) => {
+    } catch (e) {
         res.status(500).send()
-    }) 
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+// app.get('/tasks', (req, res) => {
+//     Task.find({}).then((tasks) => {
+//         res.send(tasks)
+//     }).catch((error) => {
+//         res.status(500).send()
+//     }) 
+// })
+
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
 
         res.send(task)
-    }).catch((error) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
+
+// app.get('/tasks/:id', (req, res) => {
+//     const _id = req.params.id
+
+//     Task.findById(_id).then((task) => {
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(task)
+//     }).catch((error) => {
+//         res.status(500).send()
+//     })
+// })
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
