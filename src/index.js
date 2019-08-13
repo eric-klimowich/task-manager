458ae375c9d32fd13efe8a15f67bc806cadb0b92
eric-klimowich <eklimowich@hotmail.com>
2019-08-13 07:10:01 -0400

    Created task with error handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,6 +15,18 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         res.send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    // console.log(req.body)
+    // res.send('testing!')
+
+    task.save().then(() => {
+        res.send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
