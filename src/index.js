2f00aa37c8499f83d6d6678eb3a014328aa13856
eric-klimowich <eklimowich@hotmail.com>
2019-08-13 13:34:31 -0400

    Refactored calls to user database using async and await

--- src/index.js
+++ src/index.js
@@ -8,40 +8,74 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    // console.log(req.body)
-    // res.send('testing')
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+// app.post('/users', (req, res) => {
+//     const user = new User(req.body)
+
+//     user.save().then(() => {
+//         res.status(201).send(user)
+//     }).catch((error) => {
+//         res.status(400).send(error)
+//     })
+// })
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((error) => {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-    // console.log(req.params)
+// app.get('/users', (req, res) => {
+//     User.find({}).then((users) => {
+//         res.send(users)
+//     }).catch((error) => {
+//         res.status(500).send()
+//     })
+// })
 
-    User.findById(_id).then((user) => {
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
-    }).catch((error) => {
+    } catch(e) {
         res.status(500).send()
-    })
+    }
 })
+
+// app.get('/users/:id', (req, res) => {
+//     const _id = req.params.id
+//     // console.log(req.params)
+
+//     User.findById(_id).then((user) => {
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     }).catch((error) => {
+//         res.status(500).send()
+//     })
+// })
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
