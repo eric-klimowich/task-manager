1d42ac4e8c636278ec0c88d851c95fbc30f07936
eric-klimowich <eklimowich@hotmail.com>
2019-08-13 06:44:23 -0400

    Created file for user model, moved user model to new file, included requires and exported User

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,45 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"!')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid!')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number!')
-            }
-        }
-    }
-})
-
 // const me = new User({
 //     name: '     Eric        ',
 //     email: '      EKLIMOWICH@GMAIL.COM      ',
